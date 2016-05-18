133fb31708e0cea58130614409030864c4471d9d
nomad23541 <chrisreading.email@gmail.com>
2016-05-18 17:52:47 -0600

    Added class for viewing comments, just a skelly

---
+++ src/com/chrisreading/cmdreddit/view/CommentsView.java
@@ -0,0 +1,15 @@
+package com.chrisreading.cmdreddit.view;
+
+import com.chrisreading.cmdreddit.Main;
+
+/**
+ * View for comments
+ */
+public class CommentsView extends View {
+	
+	public CommentsView(Main main) {
+		super(main);
+		
+	}
+
+}
